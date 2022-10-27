cdb9675df5bd51e43167a2fdf79c381935917454
AnARuiOfArrays <111813741+AnARuiOfArrays@users.noreply.github.com>
2022-10-27 14:20:44 -0400

    Update lambda_function.py

--- lambda_function.py
+++ lambda_function.py
@@ -3,13 +3,23 @@ import boto3
 import os
 
 def lambda_handler(event, context):
-    # TODO implement
-    
+
     client = boto3.client('dynamodb')
     
-    table_name = os.getenv('DYNAMODB_TABLE')
+    table = os.getenv('DYNAMODB_TABLE')
     
-    response = client.update_item(
+    get_item_response = client.get_item(
+        TableName = table,
+        Key = {
+            'visitor_type': {
+                'S': 'web',
+            },
+        },
+        ProjectionExpression= 'visitor_count',
+    )
+    print(get_item_response)
+    
+    update_item_response = client.update_item(
         ExpressionAttributeValues = {
             ':val': {
                 'N': '1',
@@ -20,10 +30,34 @@ def lambda_handler(event, context):
                 'S': 'web',
             },
         },
-        TableName = table_name,
+        ReturnValues = 'UPDATED_NEW',
+        TableName = table,
         UpdateExpression = 'SET visitor_count = visitor_count + :val',
     )
+    print(update_item_response)
+    
+    """
+    Test value should equal true because old value should be less than new value
+    """
+    print(get_item_response['Item']['visitor_count']['N'] + " < " + 
+    update_item_response['Attributes']['visitor_count']['N'])
+    
+    assert (get_item_response['Item']['visitor_count']['N'] < 
+    update_item_response['Attributes']['visitor_count']['N']) == True
+
+    
+    """
+    Test value should equal false because new value should not be less than old value
+    """
+    print(update_item_response['Attributes']['visitor_count']['N'] + " < " + 
+    get_item_response['Item']['visitor_count']['N'])
+    assert (update_item_response['Attributes']['visitor_count']['N'] < 
+    get_item_response['Item']['visitor_count']['N']) == False
     
     return {
-        'response': json.dumps(response),
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Origin': 'https://www.anaruiofarrays.link'
+        },
+        'body': json.dumps(update_item_response['Attributes']['visitor_count']['N']),
     }
