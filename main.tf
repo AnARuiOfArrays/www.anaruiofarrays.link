ece4673578adb4ab8e1e2b594ffbac6b2dfa5415
AnARuiOfArrays <111813741+AnARuiOfArrays@users.noreply.github.com>
2022-11-15 16:31:31 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,27 +2,25 @@ provider "aws" {
   region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.example.bucket
 
-  filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  index_document {
+    suffix = "index.html"
   }
 
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
+  error_document {
+    key = "error.html"
   }
 
-  owners = ["099720109477"] # Canonical
-}
-
-resource "aws_instance" "ubuntu" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = var.instance_type
-
-  tags = {
-    Name = var.instance_name
-  }
+  routing_rules = <<EOF
+[{
+    "Condition": {
+        "KeyPrefixEquals": "docs/"
+    },
+    "Redirect": {
+        "ReplaceKeyPrefixWith": ""
+    }
+}]
+EOF
 }
