fb633d773be2bb31be25314f3b85400c176f3234
AnARuiOfArrays <111813741+AnARuiOfArrays@users.noreply.github.com>
2022-11-17 13:49:38 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,23 +11,7 @@ resource "aws_s3_bucket" "bucket_web" {
     index_document = "index.html"
     error_document = "error.html"
   }
-}
-
-resource "aws_s3_bucket_website_configuration" "bucket_web_config" {
-  bucket = aws_s3_bucket.bucket_web.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "error.html"
-  }
-}
-
-resource "aws_s3_bucket_cors_configuration" "bucket_cors_config" {
-  bucket = aws_s3_bucket.bucket_web.id
-
+  
   cors_rule {
     allowed_methods = ["GET"]
     allowed_origins = ["*"]
