54f655feb7d7b3d7c93df03970a59ec8b9f2eae0
AnARuiOfArrays <111813741+AnARuiOfArrays@users.noreply.github.com>
2022-11-17 19:12:50 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,3 +24,36 @@ resource "aws_s3_bucket_ownership_controls" "example" {
     object_ownership = "BucketOwnerPreferred"
   }
 }
+
+resource "aws_s3_bucket_object" "bucket_web_html" {
+  bucket = aws_s3_bucket.bucket_web.id
+  key    = "index.html"
+  source = "index.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("index.html")
+}
+
+resource "aws_s3_bucket_object" "bucket_web_css" {
+  bucket = aws_s3_bucket.bucket_web.id
+  key    = "styles.css"
+  source = "styles.css"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("styles.css")
+}
+
+resource "aws_s3_bucket_object" "bucket_web_js" {
+  bucket = aws_s3_bucket.bucket_web.id
+  key    = "visitor_counter.js"
+  source = "visitor_counter.js"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("visitor_counter.js")
+}
