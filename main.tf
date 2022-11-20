251555e2ef272b642e498a2e6171db30d3119eb2
AnARuiOfArrays <111813741+AnARuiOfArrays@users.noreply.github.com>
2022-11-20 17:02:58 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ provider "aws" {
 #Create AWS S3 bucket for website
 resource "aws_s3_bucket" "bucket_web" {
   bucket = var.subdomain_web
-  policy = file("bucket_web_policy.json")
+  policy = templatefile("subdomain_web_policy.json", {bucket = var.subdomain_web})
   
   cors_rule {
     allowed_methods = ["GET"]
