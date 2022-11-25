0fc6203345d087eca05bb1ca07f54fdeef99f6d1
AnARuiOfArrays <111813741+AnARuiOfArrays@users.noreply.github.com>
2022-11-25 16:09:15 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -65,6 +65,43 @@ resource "aws_s3_bucket_ownership_controls" "domain" {
   }
 }
 
+#Create ACM certificate 
+resource "aws_acm_certificate" "certificate" {
+  domain_name       = var.domain
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#Configure Route 53
+resource "aws_route53_zone" "primary" {
+  name = var.domain
+}
+
+resource "aws_route53_record" "records" {
+  for_each = {
+    for dvo in aws_acm_certificate.certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.primary.zone_id
+}
+
+resource "aws_acm_certificate_validation" "validation" {
+  certificate_arn         = aws_acm_certificate.certificate.arn
+  validation_record_fqdns = [for record in aws_route53_record.records : record.fqdn]
+}
+
 #Create CloudFront distribution for web
 resource "aws_cloudfront_distribution" "web" {
   origin {
@@ -106,7 +143,7 @@ resource "aws_cloudfront_distribution" "web" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = var.acm_certificate_arn
+    acm_certificate_arn      = aws_acm_certificate.certificate.arn
     minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method       = "sni-only"
   }
@@ -151,14 +188,10 @@ resource "aws_cloudfront_distribution" "domain" {
   }
 
   viewer_certificate {
-    acm_certificate_arn      = var.acm_certificate_arn
+    acm_certificate_arn      = aws_acm_certificate.certificate.arn
     minimum_protocol_version = "TLSv1.2_2021"
     ssl_support_method       = "sni-only"
   }
-}
-
-resource "aws_route53_zone" "primary" {
-  name = var.domain
 }
 
 resource "aws_route53_record" "a_domain" {
@@ -183,21 +216,4 @@ resource "aws_route53_record" "a_web" {
     zone_id                = aws_cloudfront_distribution.web.hosted_zone_id
     evaluate_target_health = false
   }
-}
-
-resource "aws_route53_record" "domain_validation" {
-  for_each = {
-    for option in aws_acm_certificate.ssl_certificate.domain_validation_options : option.domain_name => {
-      name   = option.resource_record_name
-      record = option.resource_record_value
-      type   = option.resource_record_type
-    }
-  }
-
-  name    = each.value.name
-  type    = each.value.type
-  zone_id = aws_route53_zone.main.id
-  records = [each.value.record]
-  ttl     = 60
-
 }
