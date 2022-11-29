f203620872f8bbd29146303d9ad73c98198a66db
AnARuiOfArrays <111813741+AnARuiOfArrays@users.noreply.github.com>
2022-11-29 11:18:27 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,13 @@
 #Set provider as AWS
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
